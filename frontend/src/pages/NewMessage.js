a59de49079ffd6ff099679a3e518f07a1ba99fa3
boluwatifeajayi <bolu4good@gmail.com>
2023-02-17 16:08:28 +0100

    post messages

--- frontend/src/pages/NewMessage.js
+++ frontend/src/pages/NewMessage.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { createMessage } from '../features/message/messageSlice'
+
 
 function NewMessage() {
+  const [text, setText] = useState('my text')
+
+	const dispatch = useDispatch()
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		dispatch(createMessage({username:"random user", content:text}))
+		// setText('')
+	}
   return (
 	<div>
 		<div class="container2">
       <h1>Add New Message</h1>
-      <form>
-        <label for="body">Content:</label>
-        <textarea id="body" name="body" rows="8" required placeholder='start typing...'></textarea>
+      <form onSubmit={onSubmit}>
+        <label for="body">Text:</label>
+        <input id="body" name="body"  required placeholder='start typing...' value={text} onChange={(e) => setText(e.target.value)}/>
         <button type="submit">Submit</button>
       </form>
     </div>
